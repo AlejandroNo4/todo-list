ad148def0f69f1722a25e39b2039e6a9872852d9
Alejandro Contreras <kdta15@outlook.com>
2021-06-15 21:47:12 -0500

    Update webpack.config file

--- webpack.config.js
+++ webpack.config.js
@@ -16,16 +16,8 @@ const config = {
     open: true,
     host: 'localhost',
   },
-  plugins: [
-    // Add your plugins here
-    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-  ],
   module: {
     rules: [
-      {
-        test: /\.(js|jsx)$/i,
-        loader: 'babel-loader',
-      },
       {
         test: /\.css$/i,
         use: [stylesHandler, 'css-loader'],
@@ -34,9 +26,6 @@ const config = {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
         type: 'asset',
       },
-
-      // Add your rules for custom modules here
-      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
   },
 };
