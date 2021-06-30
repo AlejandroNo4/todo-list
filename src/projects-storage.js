3f15cf994216fd552ded9f94aea6b1fb1a0745f1
Alejandro Contreras <kdta15@outlook.com>
2021-06-29 20:12:26 -0500

    Add tests for projects-storage file. Change jest testEnvironment to jsdom. Add mock for window.location. Add thor error to projects-storage when creating and deleting with unexpected properties

--- src/projects-storage.js
+++ src/projects-storage.js
@@ -15,6 +15,10 @@ const allProjects = () => {
       }
       const id = projects.length + 1;
 
+      if (title === "" || title === undefined){
+        throw new Error("Title is undefined!");
+      }
+
       const newProject = new Project(title, id);
       projects.push(newProject);
 
@@ -33,6 +37,10 @@ const allProjects = () => {
         projects = JSON.parse(localStorage.getItem('projects'));
       } else {
         projects = [];
+      }
+
+      if(projectId === undefined || projectId === 1 || projectId > projects.length){
+        throw new Error("The project id to delete is invalid!");
       }
 
       for (let i = 0; i < projects.length; i += 1) {
